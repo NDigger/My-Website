f731189228d06cc320dde60717c0b7deaef86c17
NDagger <yartsiz@gmail.com>
2025-12-06 16:42:05 +0100

    header root update

--- features/header.js
+++ features/header.js
@@ -1,16 +1,18 @@
+const root = 'old/'
+
 const HTML = `
 <header>
     <div id="left-header-refs">
-        <a href="/index.html">Main Page</a>
+        <a href="${root}index.html">Main Page</a>
     </div>
     <nav id="right-header-links">
         <button id="menu-btn">Menu
             <div id="menu-btn-popup" class="header-popup" style="display: none">
-                <a href="/index.html">Main Page</a>
-                <a href="/discussions/index.html">Discussions</a>
-                <a href="/settings/index.html">Settings</a>
+                <a href="${root}index.html">Main Page</a>
+                <a href="${root}discussions/index.html">Discussions</a>
+                <a href="${root}settings/index.html">Settings</a>
                 <p class="header-category">Links</p><hr>
-                <a href="/drawings/index.html">My Drawings</a>
+                <a href="${root}drawings/index.html">My Drawings</a>
                 <a href="https://ndagger1.itch.io/" target="_blank">My Games</a>
             </div>
         </button>
